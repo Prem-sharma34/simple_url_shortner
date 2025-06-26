408ac65e09159db13a38501015609da33ac3ea75
Prem-sharma34 <ps4183085@gmail.com>
2025-06-26 23:17:59 +0530

    Build starting part of this url project

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
-from flask import Flask
-
+from flask import Flask, render_template , request,redirect,url_for
+import sqlite3
+import string
 
 app = Flask(__name__)
 
+def init_database():
+    connection = sqlite3.connect("url_db.db")
+    connection.execute("""
+            CREATE TABLE IF NOT EXISTS url(
+                       id integer primary key autoincrement,
+                       long_url text,
+                       short_url text
+                       )""")
+    connection.close()
 
-app.route("/")
-def Home():
-    return "Welcome to Home page"
+init_database()
+
+
+@app.route("/", methods=["GET","POST"]) 
+def home():
+    if request.method == 'POST':
+        url = request.form.get('url')
+        return redirect(url_for('result', url=url))
+    return render_template("Home.html")
 
 
 
+@app.route("/result")
+def result():
+    url = request.args.get('url')
+    result = url_shortner(url)
+    return render_template("result.html", result=result)
 
 
+def url_shortner(url):
+    return url
 
-if __name__ == "main":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True )
