dbcb6d95758da7e5d9a2bce6b8ddaa604b2035b6
Prem-sharma34 <ps4183085@gmail.com>
2025-06-27 23:23:30 +0530

    Build the backend logic and working of the this url project

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template , request,redirect,url_for
+from flask import Flask, render_template , request,redirect,url_for, flash
 import sqlite3
 import string
+import random
 
 app = Flask(__name__)
 
@@ -34,7 +35,43 @@ def result():
 
 
 def url_shortner(url):
-    return url
+    long_url = url
+    short_url = generate_url()
+    address = "http://127.0.0.1:5000/"
+    result= address+short_url
+    connection = sqlite3.connect('url_db.db')
+    cursor = connection.cursor()
+    if connection.execute("select short_url from url where short_url = (?)", (short_url,)) == short_url: 
+        short_url=generate_url()
+
+    cursor.execute("insert into url (long_url,short_url) values (?,?)",(long_url,short_url))
+    connection.commit()
+    connection.close()
+    return result
+
+def generate_url():
+    
+    k = 5
+    character = string.ascii_letters + string.digits
+    random_string = "".join(random.choices(character,k=5))
+    return random_string
+
+
+@app.route("/<short_url>")
+def rediret(short_url):
+    print(short_url)
+    connection = sqlite3.connect("url_db.db")
+    connection.row_factory = sqlite3.Row  # So we can access by column name
+
+    cursor = connection.execute("SELECT long_url FROM url WHERE short_url = ?", (short_url,))
+    result = cursor.fetchone()
+
+    if result:
+        long_url = result["long_url"] 
+        return redirect(long_url)
+    else:
+        return "URL not found", 404
+
 
 if __name__ == '__main__':
     app.run(debug=True )
